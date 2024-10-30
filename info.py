4a79197bf6d536350e5419486302163e92bfe10f
Tech2705 <160301845+Tech2705@users.noreply.github.com>
2024-10-30 20:29:47 +0530

    info.py

--- info.py
+++ info.py
@@ -32,7 +32,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Movie:Z3xsyG1USv32t70l@cluster0.4ogrr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://file2link:2TDBFQ2qwvafph1f@file2link.rxzed.mongodb.net/?retryWrites=true&w=majority&appName=File2Link
+")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
